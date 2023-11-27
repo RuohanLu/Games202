4e8b7c38fed866193ba790e3d7c7e0ac56a37e00
Lew <18721739579@163.com>
2023-11-26 22:19:07 -0500

    finished hw1, fixed model bug in phong shader

--- homework0/src/shaders/InternalShader.js
+++ homework0/src/shaders/InternalShader.js
@@ -80,20 +80,23 @@ const PhongVertexShader = `
 attribute vec3 aVertexPosition;
 attribute vec3 aNormalPosition;
 attribute vec2 aTextureCoord;
-uniform mat4 uModelMatrix;
-uniform mat4 uViewMatrix;
+
+uniform mat4 uModelViewMatrix;
 uniform mat4 uProjectionMatrix;
 
 varying highp vec2 vTextureCoord;
 varying highp vec3 vFragPos;
 varying highp vec3 vNormal;
+
 void main(void) {
-vFragPos = (uModelMatrix * vec4(aVertexPosition , 1.0)).xyz;
-vNormal = (uModelMatrix * vec4(aNormalPosition , 0.0)).xyz;
-gl_Position = uProjectionMatrix * uViewMatrix * uModelMatrix * vec4(aVertexPosition , 1.0);
+vFragPos = aVertexPosition;
+vNormal = aNormalPosition;
+
+gl_Position = uProjectionMatrix * uModelViewMatrix * vec4(aVertexPosition , 1.0);
 vTextureCoord = aTextureCoord;
 }
 `;
+
 const PhongFragmentShader = `
 #ifdef GL_ES
 precision mediump float;
@@ -106,9 +109,11 @@ uniform vec3 uLightPos;
 uniform vec3 uCameraPos;
 uniform float uLightIntensity;
 uniform int uTextureSample;
+
 varying highp vec2 vTextureCoord;
 varying highp vec3 vFragPos;
 varying highp vec3 vNormal;
+
 void main(void) {
 vec3 color;
 if (uTextureSample == 1) {
@@ -116,16 +121,21 @@ color = pow(texture2D(uSampler , vTextureCoord).rgb, vec3(2.2));
 } else {
 color = uKd;
 }
+
 vec3 ambient = 0.05 * color;
+
 vec3 lightDir = normalize(uLightPos - vFragPos);
 vec3 normal = normalize(vNormal);
 float diff = max(dot(lightDir , normal), 0.0);
-float light_atten_coff = uLightIntensity / pow(length(uLightPos - vFragPos), 2.0);
+float light_atten_coff = uLightIntensity / length(uLightPos - vFragPos);
 vec3 diffuse = diff * light_atten_coff * color;
+
 vec3 viewDir = normalize(uCameraPos - vFragPos);
-vec3 halfDir = normalize((lightDir + viewDir));
-float spec = pow (max(dot(halfDir , normal), 0.0), 32.0);
+float spec = 0.0;
+vec3 reflectDir = reflect(-lightDir , normal);
+spec = pow (max(dot(viewDir , reflectDir), 0.0), 35.0);
 vec3 specular = uKs * light_atten_coff * spec;
+
 gl_FragColor = vec4(pow((ambient + diffuse + specular), vec3(1.0/2.2)), 1.0);
 }
 `;
